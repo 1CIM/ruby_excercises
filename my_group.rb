06dd2beef8279ed4c1b066de13310a7d860649ba
kim_haaga <kim.haaga@live.se>
2021-03-22 21:09:34 +0100

    added person hashes to my_group array

---
+++ my_group.rb
@@ -0,0 +1,7 @@
+my_group = []
+person_1 = {name:'david', age:26, gender:'male'}
+person_2 = {name:'Lovisa', age:22, gender:'fe,ale'}
+parson_3 = {name:'Roger', age:34, gender:'male'}
+my_group << person_1
+my_group << person_2
+my_group << person_3
